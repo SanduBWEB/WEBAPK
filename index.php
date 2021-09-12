3bcc7aef5f3e5672dc795d9049c1273b1c3cfe16
DWEB SISTEM LOGISTIC <bantis.sandu@iis.utm.md>
2021-09-12 22:46:49 +0300

    test

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style/main.css" type="text/css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="main-menu-items">
+            <div class="logo">
+                <img src="include/goto.png" alt="logo" width="80em">
+
+            </div>
+                <ul class="mm-items">
+                    <li>
+                        Item 1
+                    </li>
+                    <li>
+                        Item 2
+                    </li>
+                    <li>
+                        Item 3
+                    </li>
+                </ul>
+        </div>
+    </div>
+</header>
+</body>
+</html>
