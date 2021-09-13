360fac46c0f9cafb4e121abd6e8fcc1a85c48f2f
DWEB SISTEM LOGISTIC <bantis.sandu@iis.utm.md>
2021-09-13 19:53:10 +0300

    v1.2

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+    "require_once 'include/db.php'"
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,30 +9,88 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="style/main.css" type="text/css" rel="stylesheet">
-    <title>Document</title>
+    <!-- Chorme, FireFox, Opera -->
+    <meta name="theme-color" content="#36A26B">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#36A26B">
+    <!-- Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#36A26B">
+
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" type="text/css" href="styles/adaptive.css">
+
+    <title>Shop GO - Marketplace</title>
+    <style>
+       body { overflow-x: hidden; }
+       html { background-color: #fff; }
+    </style>
 </head>
 <body>
+
 <header>
     <div class="container">
-        <div class="main-menu-items">
-            <div class="logo">
-                <img src="include/goto.png" alt="logo" width="80em">
-
-            </div>
-                <ul class="mm-items">
-                    <li>
-                        Item 1
-                    </li>
-                    <li>
-                        Item 2
-                    </li>
-                    <li>
-                        Item 3
-                    </li>
-                </ul>
+    <a href="#" class="logo"> <img src="include/goto.png" alt=""> </a>
+    <ul class="menu_top">
+        <li><a href="#">Item 1</a> </li>
+        <li><a href="#">Item 2</a> </li>
+        <li><a href="#">Item 3</a> </li>
+    </ul>
+    </div>
+    <div class="topnav">
+        <a href="#" class="active"><img src="https://d.radikal.ru/d43/2108/08/8c9b77d73bf4.png" alt="logomobile"></a>
+        <div id="myLinks">
+            <a href="#">Item unu</a>
+            <a href="#">Item doi</a>
+            <a href="#">Item trei</a>
         </div>
+        <a href="javascript:void(0);" class="icon" onclick="phoneNav()">
+            <img src="https://static.thenounproject.com/png/592441-200.png" alt="mobilemenu">
+        </a>
     </div>
 </header>
+
+
+
+<section id="content">
+    <div class="container">
+        <div class="inner_content">
+            <h2>Descrierea obtiunilor</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+    </div>
+</section>
+
+
+
+<footer>
+    <div class="container">
+        <nav class="m_mooter">
+            <ul>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+                <li><a href="#">Parametru</a></li>
+            </ul>
+        </nav>
+        <div class="author">
+            <a href="#">Powered BY. Nicu & Sandu © <?php echo date("Y"); ?></a>
+        </div>
+    </div>
+</footer>
+<script>
+    function phoneNav() {
+        const x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
+</script>
 </body>
 </html>
